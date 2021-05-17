06750f4db2ccacf858e9d70c29b821917176ccac
Chappelletto <wereismymind13@gmail.com>
2021-05-17 11:08:29 +0400

    17.05.2021

---
+++ hash_ex.rb
@@ -0,0 +1,2 @@
+
+users_hash = { name: 'peter', second_name: 'martin'}
--- lesson_13_files/chapter_54.rb
+++ lesson_13_files/chapter_54.rb
@@ -1,7 +1,27 @@
+# encoding: utf-8
+
+
 file = File.new('./data/quotes.txt', 'r:UTF-8')
 
-lines = file.readlines.size
+#count_str = file.readlines.size
+lines = file.readlines
 
-lines
+empty_string = 0
 
-puts "Всего строк #{lines}"
+
+
+lines.each {|x| if x == "\n" then empty_string += 1 end}
+
+=begin
+lines.each_with_index do |line, index|
+  # Если строка состоит из одного символа "\n", увеличиваем счетчик пустых
+  # строк.
+  if line == "\n"
+    empty_string += 1
+  end
+end 
+=end
+
+
+#puts "Всего строк #{count_str}"
+puts empty_string
